b93b0387caee7329e215ece362e57c1ad8cb7e57
vamsi <gedelavamsi10824@gmail.com>
2026-04-30 16:39:15 +0530

    Add files via upload

--- Queries.sql
+++ Queries.sql
@@ -1,185 +1,109 @@
-# SQL Project: Data Analysis for Zomato - A Food Delivery Company
+--EDA
 
-## Overview
+select * from customers;
+select * from restaurants;
+select * from orders;
+select * from riders;
+select * from deliveries;
 
-This project demonstrates my SQL problem-solving skills through the analysis of data for Zomato, a popular food delivery company in India. The project involves setting up the database, importing data, handling null values, and solving a variety of business problems using complex SQL queries.
 
-## Project Structure
+-- Handling null values
+select count(*) from customers
+where reg_date is null or customer_name is null;
 
-- **Database Setup:** Creation of the `zomato_db` database and the required tables.
-- **Data Import:** Inserting sample data into the tables.
-- **Data Cleaning:** Handling null values and ensuring data integrity.
-- **Business Problems:** Solving 20 specific business problems using SQL queries.
 
-![ERD](https://github.com/Monishbala/Zomata/blob/main/image.png)
+select count(*) from restaurants
+where restaurant_name is null or city is null or opening_hours is null;
 
-## Database Setup
-```sql
-CREATE DATABASE zomato_db;
-```
+select count(*) from orders
+where order_item is null or order_date is null or total_amount is null or order_time is null or order_status is null;
 
-### 1. Dropping Existing Tables
-```sql
-DROP TABLE IF EXISTS deliveries;
-DROP TABLE IF EXISTS Orders;
-DROP TABLE IF EXISTS customers;
-DROP TABLE IF EXISTS restaurants;
-DROP TABLE IF EXISTS riders;
+select count(*) from riders
+where rider_name is null or sign_up is null;
 
--- 2. Creating Tables
-CREATE TABLE restaurants (
-    restaurant_id SERIAL PRIMARY KEY,
-    restaurant_name VARCHAR(100) NOT NULL,
-    city VARCHAR(50),
-    opening_hours VARCHAR(50)
-);
+select count(*) from deliveries
+where restaurant_name is null or city is null or opening_hours is null;
 
-CREATE TABLE customers (
-    customer_id SERIAL PRIMARY KEY,
-    customer_name VARCHAR(100) NOT NULL,
-    reg_date DATE
-);
 
-CREATE TABLE riders (
-    rider_id SERIAL PRIMARY KEY,
-    rider_name VARCHAR(100) NOT NULL,
-    sign_up DATE
-);
+--Analysis & Reports
 
-CREATE TABLE Orders (
-    order_id SERIAL PRIMARY KEY,
-    customer_id INT,
-    restaurant_id INT,
-    order_item VARCHAR(255),
-    order_date DATE NOT NULL,
-    order_time TIME NOT NULL,
-    order_status VARCHAR(20) DEFAULT 'Pending',
-    total_amount DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
-    FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id)
-);
+-- Q.1
+-- Write a query to find the top 5 most frequently ordered dishes by customer called "Arjun Mehta" in the last 1 year.
 
-CREATE TABLE deliveries (
-    delivery_id SERIAL PRIMARY KEY,
-    order_id INT,
-    delivery_status VARCHAR(20) DEFAULT 'Pending',
-    delivery_time TIME,
-    rider_id INT,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (rider_id) REFERENCES riders(rider_id)
-);
-```
+select * from 
+(select c.customer_id,c.customer_name,o.order_item,count(order_id) as total_orders,dense_rank() over(order by count(*) desc) as rank from customers as c
+join 
+orders as o
+on o.customer_id=c.customer_id
+where c.customer_name='Arjun Mehta' and o.order_date >= CURRENT_DATE - interval '5 Year'
+group by 1,2,3
+order by 1,4 desc) as t1
+where rank<=5;
 
-## Data Import
 
-## Data Cleaning and Handling Null Values
-
-Before performing analysis, I ensured that the data was clean and free from null values where necessary. For instance:
-
-```sql
-UPDATE orders
-SET total_amount = COALESCE(total_amount, 0);
-```
-
-## Business Problems Solved
-
-### 1. Write a query to find the top 5 most frequently ordered dishes by customer called "Arjun Mehta" in the last 1 year.
-
-```sql
-SELECT 
-	customer_name,
-	dishes,
-	total_orders
-FROM -- table name
-	(SELECT 
-		c.customer_id,
-		c.customer_name,
-		o.order_item as dishes,
-		COUNT(*) as total_orders,
-		DENSE_RANK() OVER(ORDER BY COUNT(*) DESC) as rank
-	FROM orders as o
-	JOIN
-	customers as c
-	ON c.customer_id = o.customer_id
-	WHERE 
-		o.order_date >= CURRENT_DATE - INTERVAL '1 Year'
-		AND 
-		c.customer_name = 'Arjun Mehta'
-	GROUP BY 1, 2, 3
-	ORDER BY 1, 4 DESC) as t1
-WHERE rank <= 5
-```
-
-### 2. Popular Time Slots
+-- 2. Popular Time Slots
 -- Question: Identify the time slots during which the most orders are placed. based on 2-hour intervals.
 
-**Approach 1:**
+--Approach
 
-```sql
--- Approach 1
-SELECT 
-	FLOOR(EXTRACT(HOUR FROM order_time)/2)*2 as start_time,
-	FLOOR(EXTRACT(HOUR FROM order_time)/2)*2 + 2 as end_time,
-	COUNT(*) as total_orders
-FROM orders
-GROUP BY 1, 2
-ORDER BY 3 DESC;
-```
+select
+	case 
+		when extract(hour from order_time) between 0 and 1 then '00:00- 02:00'
+		when extract(hour from order_time)  between 2 and 3 then '02:00- 04:00'
+		when extract(hour from order_time)  between 4 and 5 then '04:00- 06:00'
+		when extract(hour from order_time)  between 6 and 7 then '06:00- 08:00'
+		when extract(hour from order_time)  between 8 and 9 then '08:00- 10:00'
+		when extract(hour from order_time)  between 10 and 11 then '10:00- 12:00'
+		when extract(hour from order_time)  between 12 and 13 then '12:00- 14:00'
+		when extract(hour from order_time)  between 14 and 15 then '14:00- 16:00'
+		when extract(hour from order_time)  between 16 and 17 then '16:00- 18:00'
+		when extract(hour from order_time)  between 18 and 19 then '18:00- 20:00'
+		when extract(hour from order_time)  between 20 and 21 then '20:00- 22:00'
+		when extract(hour from order_time) between 22 and 23 then '22:00- 00:00'
+	end as time_slot,
+	count(order_id) as order_count
+from orders
+group by time_slot
+order by order_count desc;
 
-**Approach 2:**
+--approact 2
 
-```sql
-SELECT
-    CASE
-        WHEN EXTRACT(HOUR FROM order_time) BETWEEN 0 AND 1 THEN '00:00 - 02:00'
-        WHEN EXTRACT(HOUR FROM order_time) BETWEEN 2 AND 3 THEN '02:00 - 04:00'
-        WHEN EXTRACT(HOUR FROM order_time) BETWEEN 4 AND 5 THEN '04:00 - 06:00'
-        WHEN EXTRACT(HOUR FROM order_time) BETWEEN 6 AND 7 THEN '06:00 - 08:00'
-        WHEN EXTRACT(HOUR FROM order_time) BETWEEN 8 AND 9 THEN '08:00 - 10:00'
-        WHEN EXTRACT(HOUR FROM order_time) BETWEEN 10 AND 11 THEN '10:00 - 12:00'
-        WHEN EXTRACT(HOUR FROM order_time) BETWEEN 12 AND 13 THEN '12:00 - 14:00'
-        WHEN EXTRACT(HOUR FROM order_time) BETWEEN 14 AND 15 THEN '14:00 - 16:00'
-        WHEN EXTRACT(HOUR FROM order_time) BETWEEN 16 AND 17 THEN '16:00 - 18:00'
-        WHEN EXTRACT(HOUR FROM order_time) BETWEEN 18 AND 19 THEN '18:00 - 20:00'
-        WHEN EXTRACT(HOUR FROM order_time) BETWEEN 20 AND 21 THEN '20:00 - 22:00'
-        WHEN EXTRACT(HOUR FROM order_time) BETWEEN 22 AND 23 THEN '22:00 - 00:00'
-    END AS time_slot,
-    COUNT(order_id) AS order_count
-FROM Orders
-GROUP BY time_slot
-ORDER BY order_count DESC;
-```
+select
+	floor(extract(hour from order_time)/2)*2 as start_time,
+	floor(extract(hour from order_time)/2)*2 +2 as end_time,
+	count(*) as order_count
+from orders
+group by start_time,end_time
+order by order_count desc;
 
-### 3. Order Value Analysis
+
+-- 3. Order Value Analysis
 -- Question: Find the average order value per customer who has placed more than 750 orders.
 -- Return customer_name, and aov(average order value)
 
-```sql
+
 select c.customer_name,avg(o.total_amount) as aov 
 from orders o join customers c
 on c.customer_id=o.customer_id
 group by 1
 having count(order_id)>750;
-```
 
-### 4. High-Value Customers
+
+-- 4. High-Value Customers
 -- Question: List the customers who have spent more than 100K in total on food orders.
 -- return customer_name, and customer_id!
-
-```sql
 select c.customer_name,sum(o.total_amount) as total_spent
 from orders o join customers c
 on c.customer_id=o.customer_id
 group by 1
 having sum(o.total_amount) >100000;
-```
 
-### 5. Orders Without Delivery
+
+-- 5. Orders Without Delivery
 -- Question: Write a query to find orders that were placed but not delivered. 
 -- Return each restuarant name, city and number of not delivered orders 
 
-```sql
--- Approach 1
+
 SELECT 
 	r.restaurant_name,
 	COUNT(o.order_id) as cnt_not_delivered_orders
@@ -192,28 +116,13 @@ deliveries as d
 ON d.order_id = o.order_id
 WHERE d.delivery_id IS NULL
 GROUP BY 1
-ORDER BY 2 DESC
-
--- Approach 2
-SELECT 
-	r.restaurant_name,
-	COUNT(*)
-FROM orders as o
-LEFT JOIN 
-restaurants as r
-ON r.restaurant_id = o.restaurant_id
-WHERE 
-	o.order_id NOT IN (SELECT order_id FROM deliveries)
-GROUP BY 1
-ORDER BY 2 DESC
-```
+ORDER BY 2 DESC;
 
 
-### 6. Restaurant Revenue Ranking: 
+-- Q. 6
+-- Restaurant Revenue Ranking: 
 -- Rank restaurants by their total revenue from the last year, including their name, 
 -- total revenue, and rank within their city.
-
-```sql
 select 
 	r.city,r.restaurant_name,
 	sum(o.total_amount) as revenue,
@@ -226,12 +135,10 @@ where o.order_date >= CURRENT_DATE - Interval'1 Year'
 group by 1,2
 order by 1,3 desc;
 
-```
-
-### 7. Most Popular Dish by City: 
+-- Q. 7
+-- Most Popular Dish by City: 
 -- Identify the most popular dish in each city based on the number of orders.
 
-```sql
 with most_pop_dish_per_city
 as(
 select r.city,o.order_item,count(o.order_id) as total_orders,rank() over(partition by r.city order by count(order_id) desc) as rank from orders as o
@@ -242,26 +149,24 @@ order by 1,3 desc)
 select city,order_item from most_pop_dish_per_city
 where rank =1;
 
-```
 
-### 8. Customer Churn: 
+
+-- Q.8 Customer Churn: 
 -- Find customers who haven’t placed an order in 2024 but did in 2023.
 
-```sql
-SELECT DISTINCT customer_id FROM orders
-WHERE 
-	EXTRACT(YEAR FROM order_date) = 2023
-	AND
-	customer_id NOT IN 
-					(SELECT DISTINCT customer_id FROM orders
-					WHERE EXTRACT(YEAR FROM order_date) = 2024);
-```
+select distinct customer_id 
+from orders 
+	where 
+	extract(year from order_date)=2023
+	and customer_id not in (
+							select distinct customer_id from orders where extract(year from order_date)=2024);
 
-### 9. Cancellation Rate Comparison: 
+
+
+-- Q.9 Cancellation Rate Comparison: 
 -- Calculate and compare the order cancellation rate for each restaurant between the 
 -- current year and the previous year.
 
-```sql
 with cancel_ratio as
 (
 select 
@@ -313,14 +218,12 @@ select
 from current_year_date as c
 join
 last_year as l
-on c.restaurant_id=l.restaurant_id 
-    
-```
+on c.restaurant_id=l.restaurant_id
 
-### 10. Rider Average Delivery Time: 
+
+-- Q.10 Rider Average Delivery Time: 
 -- Determine each rider's average delivery time.
 
-```sql
 SELECT 
     d.rider_id,
     AVG(
@@ -338,12 +241,11 @@ JOIN deliveries d
 ON o.order_id = d.order_id
 WHERE d.delivery_status = 'Delivered'
 GROUP BY d.rider_id;
-```
 
-### 11. Monthly Restaurant Growth Ratio: 
+
+-- Q.11 Monthly Restaurant Growth Ratio: 
 -- Calculate each restaurant's growth ratio based on the total number of delivered orders since its joining
 
-```sql
 with growth_ratio
 as(
 select 
@@ -361,15 +263,12 @@ order by 1,2
 )
 select restaurant_id,
 	month,cnt_orders,prev_month_order,round((cnt_orders::numeric-prev_month_order::numeric)/prev_month_order::numeric*100,2) as growth_ratio from growth_ratio;
-```
 
-### 12. Customer Segmentation: 
+-- Q.12 Customer Segmentation: 
 -- Customer Segmentation: Segment customers into 'Gold' or 'Silver' groups based on their total spending 
 -- compared to the average order value (AOV). If a customer's total spending exceeds the AOV, 
 -- label them as 'Gold'; otherwise, label them as 'Silver'. Write an SQL query to determine each segment's 
 -- total number of orders and total revenue
-
-```sql
 
 select 
 	customer_level,
@@ -388,12 +287,11 @@ select
 from orders
 group by customer_id)
 group by customer_level;
-```
 
-### 13. Rider Monthly Earnings: 
+
+-- Q.13 Rider Monthly Earnings: 
 -- Calculate each rider's total monthly earnings, assuming they earn 8% of the order amount.
 
-```sql
 select 
 	d.rider_id,
 	to_char(o.order_date,'mm-yy') as month,
@@ -404,16 +302,15 @@ join deliveries as d
 on o.order_id=d.delivery_id
 group by 1,2
 order by 1,2 desc
-```
 
-### Q.14 Rider Ratings Analysis: 
+
+-- Q.14 Rider Ratings Analysis: 
 -- Find the number of 5-star, 4-star, and 3-star ratings each rider has.
 -- riders receive this rating based on delivery time.
 -- If orders are delivered less than 15 minutes of order received time the rider get 5 star rating,
 -- if they deliver 15 and 20 minute they get 4 star rating 
 -- if they deliver after 20 minute they get 3 star rating.
 
-```sql
 
 select 
 	rider_id,
@@ -443,13 +340,12 @@ from(
 ) as t2
 group by 1,2
 order by 1,3 desc;
-	
-```
 
-### 15. Q.15 Order Frequency by Day: 
+
+-- Q.15 Order Frequency by Day: 
 -- Analyze order frequency per day of the week and identify the peak day for each restaurant.
 
-```sql
+
 select *
 from(
 select 
@@ -467,12 +363,10 @@ order by 1,3 desc
 ) as t1
 where rank=1;
 
-```
 
-### 16. Customer Lifetime Value (CLV): 
+ -- Q.16 Customer Lifetime Value (CLV): 
 -- Calculate the total revenue generated by each customer over all their orders.
 
-```sql
 select c.customer_id,
 		c.customer_name,
 	sum(o.total_amount) as clv 
@@ -480,12 +374,11 @@ from orders as o
 join customers as c
 on o.customer_id=c.customer_id
 group by 1,2;
-```
 
-### 17. Monthly Sales Trends: 
+
+-- Q.17 Monthly Sales Trends: 
 -- Identify sales trends by comparing each month's total sales to the previous month.
 
-```sql
 select 
 	extract(year from order_date) as year,
 	extract(month from order_date) as month,
@@ -494,12 +387,10 @@ select
 from orders
 group by 1,2
 order by 1,2;
-```
 
-### 18. Rider Efficiency: 
+
+-- Q.18 Rider Efficiency: 
 -- Evaluate rider efficiency by determining average delivery times and identifying those with the lowest and highest averages.
-
-```sql
 with delivery_table
 as
 (
@@ -522,12 +413,11 @@ as(
 )
 select min(avg_delivery_time),max(avg_delivery_time) from riders_time
 
-```
 
-### 19. Order Item Popularity: 
+
+-- Q.19 Order Item Popularity: 
 -- Track the popularity of specific order items over time and identify seasonal demand spikes.
 
-```sql
 select 
 	order_item,
 	season,
@@ -545,10 +435,10 @@ from orders
 )
 group by 1,2
 order by 1,3 desc;
-```
 
-### 20. Rank each city based on the total revenue for last year 2023
-```sql
+-- Q.20 Rank each city based on the total revenue for last year 2023 
+
+
 SELECT 
 	r.city,
 	SUM(total_amount) as total_revenue,
@@ -558,10 +448,4 @@ JOIN
 restaurants as r
 ON o.restaurant_id = r.restaurant_id
 GROUP BY 1;
-```
 
-## Conclusion
-
-This project highlights my ability to handle complex SQL queries and provides solutions to real-world business problems in the context of a food delivery service like Zomato. The approach taken here demonstrates a structured problem-solving methodology, data manipulation skills, and the ability to derive actionable insights from data.
-#   Z o m a t o - D a t a - A n a l y s i s -  
- 
